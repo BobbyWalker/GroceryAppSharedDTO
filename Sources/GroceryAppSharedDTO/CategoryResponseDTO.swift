c83768fe9b58c397b0f27405c446ad1db48f42b8
Bobby Walker <BobbyWalker@users.noreply.github.com>
2025-01-23 13:54:19 -0600

    Update CategoryResponseDTO.swift

--- Sources/GroceryAppSharedDTO/CategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/CategoryResponseDTO.swift
@@ -8,13 +8,17 @@
 import Foundation
 
 public struct CategoryResponseDTO: Codable, @unchecked Sendable {
-    public let id: UUID
-    public let title: String
-    public let color: String
+    public let id: UUID?
+    public let title: String?
+    public let color: String?
+    public let error: Bool
+    public let errorMessage: String?
     
-    public init(id: UUID, title: String, color: String) {
+    public init(id: UUID? = nil, title: String? = nil, color: String? = nil, error: Bool = false, errorMessage: String? = nil) {
         self.id = id
         self.title = title
         self.color = color
+        self.error = error
+        self.errorMessage = errorMessage
     }
 }
